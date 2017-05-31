adf1511de7073454a656b912e55c091ac28753e4
ngLiaXL <344212231@qq.com>
2017-05-31 16:35:55 +0800

    文件加密与解密

--- app/src/main/java/com/ngliaxl/encrypt/FileVerifyActivity.java
+++ app/src/main/java/com/ngliaxl/encrypt/FileVerifyActivity.java
@@ -1,11 +1,30 @@
 package com.ngliaxl.encrypt;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.ngliaxl.encrypt.event.FileEncryptEvent;
+import com.ngliaxl.encrypt.event.FileVerifyEvent;
+import com.ngliaxl.encrypt.manage.filter.CompositeFilter;
+import com.ngliaxl.encrypt.manage.filter.HiddenFilter;
+import com.ngliaxl.encrypt.util.FileTypeUtils;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+
+/**
+ * MD5文件完整性验证
+ */
 public class FileVerifyActivity extends AppCompatActivity {
 
     @Override
@@ -24,8 +43,48 @@ public class FileVerifyActivity extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         if (ab != null) {
-            ab.setTitle("验证与签名");
+            ab.setTitle("文件完整性验证");
             ab.setDisplayHomeAsUpEnabled(true);
         }
     }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onChooseFileEvent(FileVerifyEvent event) {
+        File currentFile = new File(event.filePath);
+        FileTypeUtils.FileType fileType = FileTypeUtils.getFileType(currentFile);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_file_choose, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.file_choose) {
+            openFilePicker();
+        } else if (menuItem.getItemId() == R.id.file_delete) {
+
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+
+    private void openFilePicker() {
+
+        ArrayList<FileFilter> filters = new ArrayList<>();
+        filters.add(new HiddenFilter());
+        CompositeFilter filter = new CompositeFilter(filters);
+
+        Intent intent = new Intent(this, FileManageActivity.class);
+        intent.putExtra(FileManageActivity.ARG_FILTER, filter);
+        intent.putExtra(FileManageActivity.ARG_CLOSEABLE, true);
+        intent.putExtra(FileManageActivity.ARG_FROM_PAGE, "file_verify");
+
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/ngliaxl/encrypt/event/FileVerifyEvent.java
@@ -0,0 +1,10 @@
+package com.ngliaxl.encrypt.event;
+
+public class FileVerifyEvent {
+
+    public String filePath ;
+
+    public FileVerifyEvent(String filePath) {
+        this.filePath = filePath;
+    }
+}
